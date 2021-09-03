df3ceeaf11cfb07499f5937d9cc1b063b3947420
Timothy Hare <timothyc.hare@gmail.com>
2021-09-03 18:03:14 -0500

    course notes and labs folder

---
+++ Projects/randomInsult.js
@@ -0,0 +1,9 @@
+// Random insult generator
+
+var randomBodyParts = [head, foot, toe, neck];
+
+var randomAdjectives = [old, hairy, crasty, fat];
+
+var randomWords = [porkchop, hatchet, busted, dumb];
+
+Math.floor()
