1af192de8af64ea06ac66172764594c6dc9d3d37
Timothy Hare <Timothyc.hare@gmail.com>
2021-09-05 21:22:59 -0500

    improving elegence, still need to finish

--- Projects/rockPSGame/testing.js
+++ Projects/rockPSGame/testing.js
@@ -1,31 +1,38 @@
-// const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();
 
-// // const weapon = prompt("Choose your weapon!");
-// // console.log("${weapon} !? Very Powerful 😤")
+// // // const weapon = prompt("Choose your weapon!");
+// // // console.log("${weapon} !? Very Powerful 😤")
 
-// let weapon = prompt("Choose your weapon! bear, human or gun? ");
-// isInArray(weapon,emojis) {
-//   if 
-// }
-// console.log(`${weapon}!? Very Powerful 😤 `);
+// // let weapon = prompt("Choose your weapon! bear, human or gun? ");
+// // isInArray(weapon,emojis) {
+// //   if 
+// // }
+// // console.log(`${weapon}!? Very Powerful 😤 `);
 
-// function isInArray(value, array) {
-//   return array.indexOf(value) > -1;
-// }
+// // function isInArray(value, array) {
+// //   return array.indexOf(value) > -1;
+// // }
 
-// var emojis = ["😂", "🤣", "😒", "😁","😎", "😜", "👏", "😤","🔫","🐻","🧔"];
+// // var emojis = ["😂", "🤣", "😒", "😁","😎", "😜", "👏", "😤","🔫","🐻","🧔"];
 
 
-// setting up weapon array
+// // setting up weapon array
 var weaponOptions = ["gun","bear","human"]
 
-// // getting computers choice
-// function getComputerChoice() {
-//   let computerChoise = weaponOptions[Math.floor(Math.random() * weaponOptions.length)];
-//   console.log(computerChoise);
-//   return computerChoise;
-// }
+let userInput = prompt("Choose your weapon! bear, human or gun? ").toLowerCase();
+if (weaponOptions.indexOf(userInput) == -1) {
+    console.log("bad choice")
+} else {
+    console.log("good choice")
+}
+
+// // // getting computers choice
+// // function getComputerChoice() {
+// //   let computerChoise = weaponOptions[Math.floor(Math.random() * weaponOptions.length)];
+// //   console.log(computerChoise);
+// //   return computerChoise;
+// // }
 
 
-let computerChoise = weaponOptions[Math.floor(Math.random() * weaponOptions.length)];
-console.log(computerChoise)
+// let computerChoise = weaponOptions[Math.floor(Math.random() * weaponOptions.length)];
+// console.log(computerChoise)
