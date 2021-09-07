dec4e64cfb16d30caa8bfdff6535c770839b44a6
Timothy Hare <timothyc.hare@gmail.com>
2021-09-07 09:57:05 -0500

    adding calorie counter code

---
+++ Projects/calorieCounter/calorieCounter.js
@@ -0,0 +1,25 @@
+//determining how many calories you consumed each day of the week.
+
+daysOfWeek = ["monday", "tuesday", "wednessday", "thursday", "friday", "saterday", "sunday"]
+
+function inputCaloriesByDay(day) {
+    if (day == daysOfWeek[0]) {
+        3600
+    } else if(day == daysOfWeek[0]) {
+        3600
+    } else if(day == daysOfWeek[1]) {
+        3600
+    } else if (day == daysOfWeek[2]) {
+        3600
+    } else if(day == daysOfWeek[3]) {
+        3600
+    } else if(day == daysOfWeek[4]) {
+        3600
+    } else if(day == daysOfWeek[5]) {
+        3600
+    } else if(day == daysOfWeek[6]) {
+        3600
+    } else {
+        console.log("inpute day of week")
+    }
+}
