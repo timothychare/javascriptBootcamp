5994e1e5047cf79248d6843787ca97c2a9bb1f20
Timothy Hare <Timothyc.hare@gmail.com>
2021-09-12 11:56:42 -0500

    Finished Calorie Counter!

--- Projects/calorieCounter/calorieCounter.js
+++ Projects/calorieCounter/calorieCounter.js
@@ -1,25 +1,56 @@
 //determining how many calories you consumed each day of the week.
 
-daysOfWeek = ["monday", "tuesday", "wednessday", "thursday", "friday", "saterday", "sunday"]
+daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
 function inputCaloriesByDay(day) {
     if (day == daysOfWeek[0]) {
-        3600
-    } else if(day == daysOfWeek[0]) {
-        3600
+        return 1600
     } else if(day == daysOfWeek[1]) {
-        3600
-    } else if (day == daysOfWeek[2]) {
-        3600
-    } else if(day == daysOfWeek[3]) {
-        3600
+        return 2600
+    } else if(day == daysOfWeek[2]) {
+        return 3600
+    } else if (day == daysOfWeek[3]) {
+        return 4600
     } else if(day == daysOfWeek[4]) {
-        3600
+        return 5600
     } else if(day == daysOfWeek[5]) {
-        3600
+        return 6600
     } else if(day == daysOfWeek[6]) {
-        3600
+        return 7600
+    } else if(day == daysOfWeek[7]) {
+        return 8600
     } else {
-        console.log("inpute day of week")
+        console.log("choose a valid day")
+    }
+};
+
+function getTotalCalories() {
+    let calories = [];
+    for (let index = 0; index < daysOfWeek.length + 1; index++) {
+        calories.push(inputCaloriesByDay(daysOfWeek[index]));
+    }
+    let sum = calories.reduce((a, b) => a + b);
+    return sum;
+};
+
+
+function getIdealCalories() {
+    var idealDailyCalories = 1500;
+    return idealDailyCalories * 7;
+};
+
+function calculateHealthPlan() {
+    var actualCalories = getTotalCalories()
+    var idealCalories = getIdealCalories()
+    if (actualCalories == idealCalories) {
+        console.log ("Good Job!")
+    }
+    if (actualCalories > idealCalories) {
+        console.log ("Time to head to the gym!")
+    }
+    if (actualCalories < idealCalories) {
+        console.log ("Time for seconds!")
     }
 }
+
+calculateHealthPlan();
