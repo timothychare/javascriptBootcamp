7699e0dc258799e908b0dfe6760f532f40c2b228
Timothy Hare <Timothyc.hare@gmail.com>
2021-09-21 17:06:14 -0500

    found largest number

--- testing/findLargestNumber.js
+++ testing/findLargestNumber.js
@@ -6,29 +6,17 @@
 // Notes
 // Expect either a positive integer or zero. Don't worry about negative integers.
 
-
-function findLargestNum(arr) {
-    arr.forEach(element => {
-        let largestNumber = 0;
-        if (element > largestNumber){
-            var largestNumber = element;
-            return largestNumber;
-        }
-    });
+    function findLargestNum(arr) {
+        var largestNumber = 0;
+        for (let index = 0; index < arr.length; index++) {
+            if (arr[index] > largestNumber) {
+                largestNumber = arr[index];
+            }
+        };
+        return largestNumber;
     };
 
-    // function findLargestNum(arr) {
-    //     var largestNumber = 0;
-    //     for (let index = 0; index < arr.length; index++) {
-    //         while (arr[index] > largestNumber) {
-    //             var largestNumber = arr[index];
-    //             return largestNumber;
-    //         }
-    
-    //     };
 
-
-
-    console.log(findLargestNum([4, 5, 1, 3]));
+    console.log(findLargestNum([4, 5, 1, 3, 27]));
     console.log(findLargestNum([300, 200, 600, 150]));
     console.log(findLargestNum([1000, 1001, 857, 1]));
